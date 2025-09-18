d277ad2e8f802b5409e902ddd6c9298995a4ff4a
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2025-09-18 23:36:39 +0530

    Time: 107 ms (44.55%) | Memory: 43.2 MB (15.77%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int [] a = new int[nums.length+2];
+        a[0] = a[a.length-1] = 1;
+        for(int i = 0; i < nums.length; i++){
+            a[i+1] = nums[i];
+        }
+        int [][] dp = new int[a.length][a.length];
+        return Burst_Coin(a,0,a.length-1,dp);
+    }
+     public static int Burst_Coin(int [] a, int i, int j,int [][] dp){
+        if (i+1==j){
+            return 0;
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int k = i+1; k < j; k++) {
+            int left =  Burst_Coin(a,i,k,dp);
+            int right = Burst_Coin(a, k, j,dp);
+            int self = a[i]*a[j]*a[k];
+            ans = Math.max(ans, left+right+self);
+        }
+        return dp[i][j] = ans;
+    }
+}
