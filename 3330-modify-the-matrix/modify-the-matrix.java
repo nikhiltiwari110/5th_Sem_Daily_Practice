d5cc092cf66cf63dbbff053273e1c5244fd21c9d
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2025-09-29 13:15:03 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (40.49%) - LeetSync

---
+++ 3330-modify-the-matrix/modify-the-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            int max = -1;
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][i]>max){
+                    max = matrix[j][i];
+                }
+            }
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][i] == -1) {
+                    matrix[j][i] = max;
+                }
+            }
+        }
+        return matrix;
+        
+    }
+}
