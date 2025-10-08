76a18d85c24e320af47438147108e9edb3900e56
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2025-10-08 11:07:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (35.42%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,32 +1,17 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        List<List<Integer>> ll = new ArrayList<>();
-        List<Integer> cl = new ArrayList<>();
-        //generate all subsets and store them
-        subset(nums,0,ll,cl);
-        //take sum of xor of each subset
-        int result = 0;
-        for(List<Integer> s : ll){
-            int sum = 0;
-            for(int el : s){
-                sum = sum ^ el;
-            }
-            result += sum;
-        }
-        return result;
-
+        return subset(nums,0,0);
     }
-    public void subset(int [] nums, int i, List<List<Integer>> ll,List<Integer> cl){
+    public int subset(int [] nums, int i, int xor){
         if(i == nums.length){
-            ll.add(new ArrayList<>(cl));
-            return;
+            return xor;
         }
 
-        //pick
-        cl.add(nums[i]);
-        subset(nums,i+1,ll,cl);
-        //notpick
-        cl.remove(cl.size()-1);
-        subset(nums,i+1,ll,cl);
+        //include/pick
+        int include = subset(nums,i+1,nums[i]^xor);
+        //exclude/notpick
+        int exclude = subset(nums,i+1,xor);
+
+        return include + exclude;
     }
 }
