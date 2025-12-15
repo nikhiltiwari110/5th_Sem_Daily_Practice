da29b021e4c67ec10e39408159c15582bc908f0d
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2025-12-15 22:07:41 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (42.13%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countKeyChanges(String s) {
+        s = s.toLowerCase();
+        int x = 0;
+        for(int i = 1; i < s.length();i++){
+            if(s.charAt(i)!=s.charAt(i-1)){
+                x++;
+            }
+        }
+        return x;
+    }
+}
