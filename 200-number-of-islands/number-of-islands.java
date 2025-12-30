496fe31e0c80bb8464e9f412db8be78d00333ba8
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2025-12-30 11:58:09 +0530

    Time: 4 ms (48.54%) | Memory: 51.8 MB (74.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,25 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-    int count = 0;
-    for(int i = 0 ; i < grid.length; i++){
-        for(int j = 0; j < grid[i].length; j++){
-            if(grid[i][j]=='1'){
-                count+=1;
-                call_rec(grid,i,j);
+        int c = 0;
+        int [][] visited = new int [grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(visited[i][j]!=1 && grid[i][j] == '1'){
+                    // visited[i][j] = 1;
+                    dfs(i,j,grid,visited);
+                    c++;
+                }
             }
         }
+        return c;   
     }
-    return count;   
-    }
-    public void call_rec(char[][] grid, int i, int j){
-        if(i<0 || i >= grid.length || j<0 || j>=grid[i].length || grid[i][j] == '0'){
+    public void dfs(int r,int c,char [][] grid, int [][] visited){
+        if(r<0 || c<0 || r>=grid.length || c >= grid[0].length || visited[r][c] == 1 || grid[r][c] == '0'){
             return;
         }
-
-        grid[i][j] = '0';
-        call_rec(grid,i+1,j);//down
-        call_rec(grid,i-1,j);//up
-        call_rec(grid,i,j-1);//left
-        call_rec(grid,i,j+1);//right
+        visited[r][c] = 1;
+        //up
+        dfs(r-1,c,grid,visited);
+        //down
+        dfs(r+1,c,grid,visited);
+        //left
+        dfs(r,c-1,grid,visited);
+        //right
+        dfs(r,c+1,grid,visited);
     }
 }
