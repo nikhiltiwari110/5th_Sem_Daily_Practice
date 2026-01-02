8aea7c800e9dee3aa105b632212d0143053cafbd
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-01-02 23:42:13 +0530

    Time: 29 ms (5.38%) | Memory: 50.3 MB (5.40%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,49 +1,50 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites.length==0){
+        int n = numCourses;
+        if(prerequisites.length == 0){
             return true;
         }
+        int [] in = new int[n];
+        for(int [] e : prerequisites){
+            in[e[0]]++;
+        }
         List<List<Integer>> graph = new ArrayList<>();
-        int n = numCourses;
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0; i < n; i++){
             graph.add(new ArrayList<>());
         }
-        for(int i = 0 ; i < prerequisites.length; i++){
-            graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        for(int [] e: prerequisites){
+            // ll.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
-
         Queue<Integer> q = new LinkedList<>();
-        int [] in = new int[n];
-        for(int i = 0; i < prerequisites.length;i++){
-            in[prerequisites[i][1]]++;
-        }
         for(int i = 0; i < n; i++){
-            if(in[i]==0){
+            if(in[i] == 0){
                 q.add(i);
             }
         }
         if(q.isEmpty()){
             return false;
         }
-        ArrayList<Integer> ll = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
         while(!q.isEmpty()){
+            //remove;
             int rv = q.poll();
-            if(in[rv]==-1){
+            //ignore
+            if(in[rv]<0){
                 continue;
             }
+            a.add(rv);
             in[rv] = -1;
-            ll.add(rv);
             for(int el : graph.get(rv)){
                 in[el]--;
             }
-            for(int i = 0; i < n; i++){
+            for(int i = 0 ; i < n; i++){
                 if(in[i]==0){
                     q.add(i);
                 }
             }
         }
-        if(ll.size()==n){
+        if(a.size()==n){
             return true;
         }else{
             return false;
