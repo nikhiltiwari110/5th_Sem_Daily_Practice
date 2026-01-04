ec8e1fd11601c01c2f01e1ecd958ebf9f946c40d
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-01-04 11:49:42 +0530

    Time: 166 ms (31.18%) | Memory: 47.6 MB (5.63%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,19 +1,24 @@
 class Solution {
+    public HashMap<String,Integer> dp = new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
-        rec(0,nums,target,0);
-        return c;
+        
+        return rec(0,nums,target,0);
     }   
-    public int c = 0;
-    public void rec(int amount,int [] nums, int target,int i){
+    
+    public int rec(int amount,int [] nums, int target,int i){
         if(amount == target && i == nums.length){
-            c++; 
-            return;
+            return 1;
         }
          if(i == nums.length){
-            return;
+            return 0;
         }
-        rec(amount+nums[i],nums,target,i+1);
-        rec(amount-nums[i],nums,target,i+1);
-
+        String s = i+" "+amount;
+        if(dp.containsKey(i+" "+amount)){
+            return dp.get(s);
+        }
+        int n = rec(amount+nums[i],nums,target,i+1);
+        int m = rec(amount-nums[i],nums,target,i+1);
+        dp.put(s,n+m);
+        return n+m;
     }
 }
