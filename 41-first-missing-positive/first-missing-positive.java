dbad17e253de11b983c74eebdf28df5c9871ddbc
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-01-19 15:40:43 +0530

    Time: 16 ms (20.48%) | Memory: 90.6 MB (14.56%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,24 +1,12 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        //change the value of negative number to nums.length+1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i]<=0||nums[i]>nums.length+1){
-                nums[i] = nums.length+1;
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : nums){
+            set.add(x);
         }
-        //we will now check the in range value and product them with -1
-        for(int i = 0; i < nums.length; i++){
-            int v = Math.abs(nums[i]);
-            if(v>0 && v < nums.length+1 && nums[v-1]>0){
-                //change the value for that index;
-                nums[v-1] = nums[v-1]*-1;
-            }
-        }
-
-        //now check for the first positive idx
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i]>0){
-                return i+1;
+        for(int i = 1; i <= nums.length; i++){
+            if(!set.contains(i)){
+                return i;
             }
         }
         return nums.length+1;
