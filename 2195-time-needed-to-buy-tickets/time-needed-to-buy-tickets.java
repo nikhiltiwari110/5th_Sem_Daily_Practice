956eeb5096b60667e1dd243363aa56f9b5f665e4
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-01-27 15:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (89.09%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,22 +1,13 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0;i < tickets.length; i++){
-            q.add(i);
+        int sum = 0;
+        for(int i = 0; i < tickets.length; i++){
+           if(i<=k){
+            sum += Math.min(tickets[i],tickets[k]);
+           }else{
+            sum += Math.min(tickets[i],tickets[k]-1);
+           }
         }
-        while(!q.isEmpty()){
-            int idx = q.poll();
-            int el = tickets[idx];
-            if(el>0){
-                tickets[idx]--;
-                q.add(idx);
-                time++;
-            }
-            if(tickets[k]==0){
-                return time;
-            }
-        }
-        return -1;
+        return sum;
     }
 }
