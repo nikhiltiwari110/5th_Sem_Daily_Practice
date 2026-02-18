9ec7cb12c43be4dc2fd4826766ce8738d95a23d7
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-02-18 23:13:10 +0530

    Time: 3 ms (74.30%) | Memory: 53.7 MB (70.40%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,13 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int [] in = new int [n];
-        int [] out = new int [n];
-        for(int i = 0; i < trust.length; i++){
-            in[trust[i][1]-1]++;
-            out[trust[i][0]-1]++;
-        }
+     int [] in = new int[n];// ith person ko kitne jne trust kr rhe hai'
+     int [] out = new int[n]; // ith person kitne jno ko trust kr rha hai
+     for(int i = 0; i < trust.length; i++){
+        int u = trust[i][0]-1;
+        int v = trust[i][1]-1;
+        in[v]++;
+        out[u]++;
+     }
         for(int i = 0; i < n; i++){
-            if(in[i]==n-1 && out[i]==0){
+            if(out[i]==0 && in[i] == n-1){
                 return i+1;
             }
         }
