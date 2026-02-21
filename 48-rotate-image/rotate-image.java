112657fea7c0c3be6bc04236cfe6f1081fd8dc04
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-02-22 00:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (32.44%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,30 +1,22 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //first we have to transpose the matrix then reverse by columns
-
-        //1. transpose
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = i+1; j < matrix[i].length; j++){
-                if(j>i){
-                    //swap
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
-                }
+        //swap the elements abovwe diagonals
+        for(int i = 0; i < matrix.length;i++){
+            for(int j = i + 1; j < matrix.length; j++){
+                //swap
+                int el = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = el;
             }
         }
-
-        //2. reverse each row 
-        for(int row = 0; row<matrix.length;row++){
-            //swap
-            int st = 0;
-            int ed = matrix[row].length-1;
-            while(st<ed){
-                int t = matrix[row][st];
-                matrix[row][st] = matrix[row][ed];
-                matrix[row][ed] = t;
-                st++;
-                ed--;
+        //reverse column wise
+        int n = matrix.length-1;
+        for(int j = 0; j  <= n/2; j++){
+            for(int i = 0; i <= n;i++){
+                //swap
+                int el = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j];
+                matrix[i][n-j] = el;
             }
         }
     }
