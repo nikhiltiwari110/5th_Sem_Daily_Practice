1b1ae7bdf38b698a8968018ce0db2f0ec72466c7
Nikhil Tiwari <nikhil.tiwari1447@gmail.com>
2026-03-10 10:30:18 +0530

    Time: 101 ms (59.54%) | Memory: 265.9 MB (79.47%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> map = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            map.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         for(int [] e : edges){
-            map.get(e[0]).add(e[1]);
-            map.get(e[1]).add(e[0]);
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
         HashSet<Integer> visited = new HashSet<>();
         Queue<Integer> q = new LinkedList<>();
         q.add(source);
         while(!q.isEmpty()){
-            int rv = q.poll();
-            if(visited.contains(rv)){
-                continue;
-            }
-            visited.add(rv);
-            if(rv==destination){
-                return true;
-            }
-            for(int el:map.get(rv)){
-                if(!visited.contains(el)){
-                    q.add(el);
-                }
+            int r = q.poll();
+            if(visited.contains(r)) continue;
+            visited.add(r);
+            if(r == destination) return true;
+            for(int el : graph.get(r)){
+                if(!visited.contains(el)) q.add(el);
             }
         }
         return false;
